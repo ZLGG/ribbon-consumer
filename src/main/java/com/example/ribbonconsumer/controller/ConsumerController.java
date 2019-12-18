3e96f242ee0bd070a1e1d67d7b45a143c34e34b9
zlg <zhengfeng.zlg@dtyunxi.com>
2019-12-18 09:43:54 +0800

    Initial commit

---
+++ src/main/java/com/example/ribbonconsumer/controller/ConsumerController.java
@@ -0,0 +1,17 @@
+package com.example.ribbonconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+@RestController
+public class ConsumerController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/ribbon", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://zlg-service/hello", String.class).getBody();
+    }
+}
